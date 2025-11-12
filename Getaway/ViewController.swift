2979a341907e1d7fd8330a30070195f615972b11
andresnserna <andresnserna@icloud.com>
2025-11-11 20:36:25 -0600

    fixed a cell issue, added two more packages to the json

--- Getaway/ViewController.swift
+++ Getaway/ViewController.swift
@@ -14,17 +14,7 @@ class ViewController: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         travelPackages = loadPackages()  // Load data from JSON once
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return travelPackages.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PackageCell", for: indexPath)
-        let package = travelPackages[indexPath.row]
-        cell.textLabel?.text = package.destination_name
-        return cell
+        
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
